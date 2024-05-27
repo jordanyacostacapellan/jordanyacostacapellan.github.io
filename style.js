12947574153b75f39e1e04802289984de239912b
Jordany Acosta Capellan <109632113+jordanyacostacapellan@users.noreply.github.com>
2024-05-27 09:15:56 -0400

    Update style.js

--- style.js
+++ style.js
@@ -1,4 +1,5 @@
-// JavaScript code
+// JavaScript code for any interactive elements or functionality
 document.addEventListener('DOMContentLoaded', function () {
     // Add your JavaScript code here
+    console.log('Document loaded');
 });
