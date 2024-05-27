4c06c72937b6f469153312e8d89d111f0ab764b6
Jordany Acosta Capellan <109632113+jordanyacostacapellan@users.noreply.github.com>
2024-05-27 09:38:53 -0400

    Update style.js

--- style.js
+++ style.js
@@ -1,16 +1,9 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const sections = document.querySelectorAll('section');
-
-    // Hide all sections except the home page initially
-    sections.forEach(section => {
-        if (section.id !== 'home') {
-            section.style.display = 'none';
-        }
+function showSection(sectionId) {
+    // Hide all sections
+    document.querySelectorAll('section').forEach(section => {
+        section.style.display = 'none';
     });
 
-    // Function to show the selected section
-    function showSection(sectionId) {
-        sections.forEach(section => {
-            if (section.id === sectionId) {
-                section.style.display = 'block';
-            }
+    // Show the selected section
+    document.getElementById(sectionId).style.display = 'block';
+}
